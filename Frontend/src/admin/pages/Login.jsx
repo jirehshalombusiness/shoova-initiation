d4c635fc42a27a23e55355ef4db76eb893d41efe
Big-Stan <244668394+stanleyokyemanarthur-max@users.noreply.github.com>
2026-03-31 21:57:10 +0000

    Update Login.jsx

--- Frontend/src/admin/pages/Login.jsx
+++ Frontend/src/admin/pages/Login.jsx
@@ -25,7 +25,7 @@ export default function Login() {
 
         try {
 
-            const res = await fetch("https://shoova-initiation.onrender.com/admin/login", {
+            const res = await fetch("https://shoova-initiation-yjg3.onrender.com/admin/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
