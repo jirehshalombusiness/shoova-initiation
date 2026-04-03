bab0b9d8b154cdfabc15e0de0d18d7f7ac1775f0
Stanley Okyemanarthur <244668394+stanleyokyemanarthur-max@users.noreply.github.com>
2026-04-03 04:57:59 +0000

    admin model

--- Backend/models/Admin.js
+++ Backend/models/Admin.js
@@ -2,11 +2,16 @@ import mongoose from "mongoose";
 
 const adminSchema = new mongoose.Schema({
   email: {
-  type: String,
-  unique: true,
-  required: true
-},
-  password: String
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 export default mongoose.model("Admin", adminSchema);
