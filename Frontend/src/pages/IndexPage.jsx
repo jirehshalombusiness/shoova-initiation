7cab6970d4d017b1319d601dd1f0d5fbd36619cb
Stanley Okyemanarthur <244668394+stanleyokyemanarthur-max@users.noreply.github.com>
2026-06-11 18:00:09 +0000

    updated

--- Frontend/src/pages/IndexPage.jsx
+++ Frontend/src/pages/IndexPage.jsx
@@ -253,7 +253,7 @@ export const IndexPage = ({ className, children, variant, contentKey, ...props }
                 </div>
                 <div className="bg-white px-4 ml-10 py-2 rounded-sm shadow">
                   <span className="text-2xl md:text-4xl lg:text-5xl font-bold text-secondary">
-                    Heal the Land.
+                    RESPONSIBLE MINING.
                   </span>
                 </div>
 
@@ -261,7 +261,7 @@ export const IndexPage = ({ className, children, variant, contentKey, ...props }
 
               <div className="mb-6">
                 <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold text-white leading-tight tracking-wide">
-                  ONE STUDENT <br /> AT A TIME
+                  Sustainable <br /> Future
                 </h2>
 
                 <p className="mt-4 text-white/80 text-sm md:text-base max-w-sm">
