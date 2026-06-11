80b70190a1b9737f7827e1fea8abfa8fa61b83ae
Stanley Okyemanarthur <244668394+stanleyokyemanarthur-max@users.noreply.github.com>
2026-06-11 22:20:39 +0000

    update

--- Frontend/src/pages/IndexPage.jsx
+++ Frontend/src/pages/IndexPage.jsx
@@ -247,7 +247,7 @@ export const IndexPage = ({ className, children, variant, contentKey, ...props }
                   </span>
                 </div>
                 <div className="bg-secondary px-4 mr-16 py-2 rounded-sm shadow">
-                  <span className="text-2xl md:text-3xl lg:text-3xl font-bold text-white uppercase">
+                  <span className="text-2xl md:text-3xl lg:text-4xl font-bold text-white uppercase">
                     Sustainable Future
                   </span>
                 </div>
